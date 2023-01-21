13ff1939720ffd1f8d29bfe6e088bb8a50802852
RemiArata <50224993+RemiArata@users.noreply.github.com>
2023-01-20 20:28:54 -0700

    added page structure

--- umbrella.py
+++ umbrella.py
@@ -1,7 +1,20 @@
-from flask import Flask
+from flask import Flask, request, redirect
 
 app = Flask(__name__)
 
 @app.route('/')
-def index():
+def home() -> None:
     return '<h>Hello World</h>'
+
+@app.route('/user/<name>')
+def user(name: str) -> None:
+    return f'<h>Hello, {name.capitalize()}!</h>'
+
+@app.route('/visit')
+def visit():
+    user = request.headers.get('User-Agent')
+    return f'<h>You are visiting from: {user}</h>'
+
+@app.route('/search')
+def search():
+    return redirect('http://google.com')
